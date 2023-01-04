c9c3b1f42427539dc9a02c46b934a01244b4b1b4
dobby2024 <117992081+dobby2024@users.noreply.github.com>
2023-01-04 21:42:38 +0900

    람다표현식

--- src/day01/hello/Hello.java
+++ src/day01/hello/Hello.java
@@ -2,6 +2,8 @@ package day01.hello;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		System.out.print("Hello, World");
+		System.out.println(System.getProperty("line.separator"));
+		System.out.println("aaaa");
 	}
 }
