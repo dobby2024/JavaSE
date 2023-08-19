08f79d4847882523a811f11d24d3ca904c4190e1
dobby8282 <dobbyisfree8282@gmail.com>
2023-08-19 09:36:05 +0900

    loop 숙제

--- src/section05/LoopHomeWork.java
+++ src/section05/LoopHomeWork.java
@@ -27,19 +27,66 @@ package section05;
 ******* 
   
 4번
-			
-   *   		
-  ***		
- *****		
-*******		
- *****		
-  ***		
-   *
+			i	j
+   *   		0   J > 3 + i
+  ***		1	J > 3 + i
+ *****		2	J > 3 + i
+*******		3	
+ *****		4	j > 9 - i
+  ***		5	j > 9 - i
+   *		6	j > 9 - i
    		
 */
 public class LoopHomeWork {
+	
+	public static void main(String[] args) {
+		
+//		for(int i = 0; i < 7; i++) {
+//			for(int j = 0; j < 7; j++) {
+//				if(i <= 7/2) {
+//					if(j < 3 - i) {
+//						System.out.print(" ");
+//					} else if (j > 3 + i) {
+//						System.out.print(" ");
+//					} else {
+//						System.out.print("*");
+//					}
+//				} else {
+//					if(j < i - 3) {
+//						System.out.print(" ");
+//					} else if (j > 9 - i) {
+//						System.out.print(" ");
+//					} else {
+//						System.out.print("*");
+//					}
+//				}
+//			}
+//			System.out.println();	// 개행
+//		}
+		
+		System.out.println("   ☆");
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 7; j++) {
+				if(j < 3 - i) {
+					System.out.print(" ");
+				} else if (j > 3 + i) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+				
+			}
+			System.out.println();	// 개행
+		}
+		System.out.println("  | | Merry Chrismas ~*");
+	}
 
 }
+
+
+
+
+
 
 
 
