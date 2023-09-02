0ee00170a6abfdc7275a63aefed069c6c09c306c
ITSC <sca@koreaedugroup.com>
2023-09-02 12:51:23 +0900

    javalang

--- src/section14/access2/InputErrorException.java
+++ src/section14/access2/InputErrorException.java
@@ -2,6 +2,8 @@ package section14.access2;
 
 public class InputErrorException extends Exception{
 	
+	private static final long serialVersionUID = 1L;
+	
 	private String message;
 	
 	public InputErrorException(String message) { // message = "입력이 잘못되었습니다."
