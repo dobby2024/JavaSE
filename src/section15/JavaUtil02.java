b7e31449856fbc9253c232aced7f3df3c39d48c9
ITSC <sca@koreaedugroup.com>
2023-09-03 12:03:30 +0900

    java util

--- src/section15/JavaUtil02.java
+++ src/section15/JavaUtil02.java
@@ -4,15 +4,19 @@ import java.util.Random;
 
 public class JavaUtil02 {
 	public static void main(String[] args) {
+		
+		// int 45크기 배열 생성
 		int[] lNum = new int[45];
 		
+		// 1부터 45까지 배열에 저장
 		for(int i = 0; i < lNum.length; i++) {
 			lNum[i] = i + 1;
 		}
 		
 		Random ran = new Random();
-		
+				
 		for(int i = 0; i < lNum.length; i++) {
+			// 0~44 숫자중 임의 값 반환
 			int changeIndex = ran.nextInt(45);
 			
 			int tmp = lNum[i];
@@ -21,11 +25,9 @@ public class JavaUtil02 {
 		}
 		
 		for(int i = 0; i < 6; i++) {
-			System.out.println(lNum[i]+ "/");
+			System.out.print(lNum[i]+ "/");
 		}
-		
 	}
-
 }
 
 
