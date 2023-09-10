75eb9a52700c44d032c0cc3da97e38f67c421a7e
ITSC <sca@koreaedugroup.com>
2023-09-10 11:26:12 +0900

    thread

---
+++ src/section18/Thread04.java
@@ -0,0 +1,16 @@
+package section18;
+
+public class Thread04 {
+	public static void main(String[] args) {
+		System.out.println("메인 스레드 시작!");
+		
+		InterThread interThread = new InterThread();
+		Thread thread = new Thread(interThread);
+		
+		thread.start();
+		
+		
+		System.out.println("메인 스레드 종료!");
+	}
+
+}
